1df34fa30bf4c579d47b43661211cb17460fdca6
SamuelFoc <samo.sipikal@gmail.com>
2023-03-07 13:13:51 +0100

    Add NodeJS project

---
+++ BackEnd/NodeJS/Project/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello, world!");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("This is the about page.");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.write("This is the contact page.");
+    res.end();
+  } else {
+    res.write("404 Not Found");
+    res.end();
+  }
+});
+
+const port = 3001;
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
